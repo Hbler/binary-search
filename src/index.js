075db3657cb62c41f278d1fcb4e7f930c6efa71d
Hugo Ernesto <mail.hbler@gmail.com>
2022-07-28 18:54:02 -0300

    feat: article concluded

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // assinatura da função
-// function localizarCarta(cartas, numN) {}
+// function localizarCarta(cartas, pesquisa) {}
 
 // Dica básica sobre a assinatura:
 // - nomeie a função e as variáveis de forma apropriada,
 //   para evitar confsão ao longo do desenvolvimento.
 
 // // versão 01
-// export function localizarCarta(cartas, numN) {
+// export function localizarCarta(cartas, pesquisa) {
 //   // Criar um loop para repetição
 //   for (let posicao in cartas) {
-//     // verificar se o elemento atual é igual à numN
-//     if (cartas[posicao] === numN) {
+//     // verificar se o elemento atual é igual à pesquisa
+//     if (cartas[posicao] === pesquisa) {
 //       // Resposta encontrada! Return e encerrar a função...
 //       return posicao;
 //     }
@@ -20,21 +20,21 @@
 // }
 
 // versão 01.1
-// export function localizarCarta(cartas, numN) {
-//   // Criar um loop para repetição
-//   for (let posicao = 0; posicao < cartas.length; posicao++) {
-//     // verificar se o elemento atual é igual à numN
-//     if (cartas[posicao] === numN) {
-//       // Resposta encontrada! Return e encerrar a função...
-//       return posicao;
-//     }
-//   }
-//   // loop acabou e não encerrou a função; enceraremos retornando o -1
-//   return -1;
-// }
+export function localizarCartaLinear(cartas, pesquisa) {
+  // Criar um loop para repetição
+  for (let posicao = 0; posicao < cartas.length; posicao++) {
+    // verificar se o elemento atual é igual à pesquisa
+    if (cartas[posicao] === pesquisa) {
+      // Resposta encontrada! Return e encerrar a função...
+      return posicao;
+    }
+  }
+  // loop acabou e não encerrou a função; enceraremos retornando o -1
+  return -1;
+}
 
 // versão 02
-// const localizarCarta = (cartas, numN) => {
+// const localizarCarta = (cartas, pesquisa) => {
 //   // verificamos se o array está vazio e retornamos -1
 //   if (cartas.length === 0) return -1;
 
@@ -48,13 +48,13 @@
 //     let cartaAtual = cartas[posicao];
 
 //     // verificar se encontramos
-//     if (cartaAtual === numN) {
+//     if (cartaAtual === pesquisa) {
 //       return posicao;
-//       // verificar se passamos do numN
-//     } else if (cartaAtual < numN) {
+//       // verificar se passamos do pesquisa
+//     } else if (cartaAtual < pesquisa) {
 //       fim = posicao - 1;
-//       // verificar se estamos antes do numN
-//     } else if (cartaAtual > numN) {
+//       // verificar se estamos antes do pesquisa
+//     } else if (cartaAtual > pesquisa) {
 //       ini = posicao + 1;
 //     }
 //   }
@@ -62,125 +62,122 @@
 //   return -1;
 // };
 
-const testarPosicao = (arr, num, index) => {
-  const carta = arr[index];
+// const testarPosicao = (arr, num, index) => {
+//   const carta = arr[index];
 
-  // verificar se posicao atual contém o numN
-  if (carta === num) {
-    // verificar se o índice anterior também contém o numN
-    // se tiver retornamos antes, para avisar que ainda não aachamos a primeira ocorrencia
-    // se não, retornamos achou, para avisar que é a primeira ocorrência
-    return index - 1 >= 0 && arr[index - 1] === num ? "antes" : "achou";
-  } // verifica se posicao atual é menor que o numN
-  else if (carta < num) {
-    // se for retornamos antes, para avisar que ainda achamos o numN
-    // e temos que procurar antes dessa posicao
-    return "antes";
-  } else {
-    // se não for retornamos depois, para avisar que ainda achamos o numN
-    // e temos que procurar depois dessa posicão
-    return "depois";
-  }
-};
+//   // verificar se posicao atual contém a pesquisa
+//   if (carta === num) {
+//     // verificar se o índice anterior também contém a pesquisa
+//     // se tiver retornamos antes, para avisar que ainda não achamos a primeira ocorrencia
+//     // se não, retornamos achou, para avisar que é a primeira ocorrência
+//     return index - 1 >= 0 && arr[index - 1] === num ? "antes" : "achou";
+//   } // verifica se posicao atual é menor que a pesquisa
+//   else if (carta < num) {
+//     // se for retornamos antes, para avisar que ainda achamos a pesquisa
+//     // e temos que procurar antes dessa posicao
+//     return "antes";
+//   } else {
+//     // se não for retornamos depois, para avisar que ainda achamos a pesquisa
+//     // e temos que procurar depois dessa posicão
+//     return "depois";
+//   }
+// };
 
 // versão 02.1
-export const localizarCarta = (cartas, numN) => {
-  // verificamos se o array está vazio e retornamos -1
-  if (cartas.length === 0) return -1;
+// export const localizarCarta = (cartas, pesquisa) => {
+//   // verificamos se o array está vazio e retornamos -1
+//   if (cartas.length === 0) return -1;
 
-  let ini = 0;
-  let fim = cartas.length;
+//   let ini = 0;
+//   let fim = cartas.length;
 
-  // Criar um loop para repetição
+//   // Criar um loop para repetição
+//   while (ini <= fim) {
+//     //encontrar o meio da lista
+//     let posicao = Math.floor((ini + fim) / 2); // o meio
+//     let resultado = testarPosicao(cartas, pesquisa, posicao);
+
+//     // verificar se encontramos
+//     if (resultado === "achou") {
+//       return posicao;
+//     } else if (resultado === "antes") {
+//       fim = posicao - 1;
+//     } else if (resultado === "depois") {
+//       ini = posicao + 1;
+//     }
+//   }
+//   // loop acabou e não encerrou a função; enceraremos retornando o -1
+//   return -1;
+// };
+
+// Algoritmo genérico da pesquisa binária
+function pesquisaBinaria(ini, fim, condicao) {
+  if (ini === fim) return -1;
+
   while (ini <= fim) {
-    //encontrar o meio da lista
-    let posicao = Math.floor((ini + fim) / 2); // o meio
-    let resultado = testarPosicao(cartas, numN, posicao);
+    let posicao = Math.floor((ini + fim) / 2);
+    let resultado = condicao(posicao);
 
-    // verificar se encontramos
     if (resultado === "achou") {
       return posicao;
     } else if (resultado === "antes") {
       fim = posicao - 1;
-    } else if (resultado === "depois") {
+    } else {
       ini = posicao + 1;
     }
   }
-  // loop acabou e não encerrou a função; enceraremos retornando o -1
+
   return -1;
+}
+
+export const localizarCarta = (cartas, pesquisa) => {
+  const condicao = (posicao) => {
+    if (cartas[posicao] === pesquisa) {
+      return posicao > 0 && cartas[posicao - 1] === pesquisa
+        ? "antes"
+        : "achou";
+    } else if (cartas[posicao] < pesquisa) {
+      return "antes";
+    } else {
+      return "depois";
+    }
+  };
+
+  return pesquisaBinaria(0, cartas.length, condicao);
 };
 
-// array de testes
-export const testes = [
-  {
-    entrada: {
-      cartas: [13, 12, 11, 7, 4, 3, 1, 0],
-      numN: 7,
-    },
-    saida: 3,
-  },
-  {
-    // O numN está em algum ponto no meio da lista cartas.
-    entrada: {
-      cartas: [13, 11, 10, 7, 4, 3, 1, 0],
-      numN: 3,
-    },
-    saida: 5,
-  },
-  {
-    // O numN é o primeiro elemento da lista cartas.
-    entrada: {
-      cartas: [4, 2, 1, -1],
-      numN: 4,
-    },
-    saida: 0,
-  },
-  {
-    // O numN é o último elemento da lista cartas.
-    entrada: {
-      cartas: [3, -1, -9, -127],
-      numN: -127,
-    },
-    saida: 3,
-  },
-  {
-    // A lista cartas tem apenas um elemento, que é o numN.
-    entrada: {
-      cartas: [6],
-      numN: 6,
-    },
-    saida: 0,
-  },
-  {
-    // A lista cartas não contém o numN.
-    entrada: {
-      cartas: [9, 7, 5, 2, -9],
-      numN: 4,
-    },
-    saida: -1, // Como o problema não especifica essa situação, definimos um valor arbitrário
-  },
-  {
-    // A lista cartas está vazia.
-    entrada: {
-      cartas: [],
-      numN: 7,
-    },
-    saida: -1,
-  },
-  {
-    // A lista cartas tem números que se repetem.
-    entrada: {
-      cartas: [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
-      numN: 3,
-    },
-    saida: 7,
-  },
-  {
-    // O numN está em mais de uma posição na lista cartas.
-    entrada: {
-      cartas: [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
-      numN: 6,
-    },
-    saida: 2,
-  },
-];
+// Aplicando em outro problema
+
+const primeiraOcorrencia = (nums, target) => {
+  const condicao = (posicao) => {
+    if (nums[posicao] === target) {
+      return posicao > 0 && nums[posicao - 1] === target ? "antes" : "achou";
+    } else if (nums[posicao] < target) {
+      return "depois";
+    } else {
+      return "antes";
+    }
+  };
+
+  return pesquisaBinaria(0, nums.length, condicao);
+};
+
+const ultimaOcorrencia = (nums, target) => {
+  const condicao = (posicao) => {
+    if (nums[posicao] === target) {
+      return posicao < nums.length - 1 && nums[posicao + 1] === target
+        ? "depois"
+        : "achou";
+    } else if (nums[posicao] < target) {
+      return "depois";
+    } else {
+      return "antes";
+    }
+  };
+
+  return pesquisaBinaria(0, nums.length, condicao);
+};
+
+const priemiraEUltimaOcorrencia = (nums, target) => {
+  return primeiraOcorrencia(nums, target), ultimaOcorrencia(nums, target);
+};
--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -1,4 +1,78 @@
-import { localizarCarta, testes } from "../";
+import { localizarCarta, localizarCartaLinear } from "../";
+
+const testes = [
+  {
+    entrada: {
+      cartas: [13, 12, 11, 7, 4, 3, 1, 0],
+      pesquisa: 7,
+    },
+    saida: 3,
+  },
+  {
+    // a pesquisa está em algum ponto no meio da lista cartas.
+    entrada: {
+      cartas: [13, 11, 10, 7, 4, 3, 1, 0],
+      pesquisa: 3,
+    },
+    saida: 5,
+  },
+  {
+    // a pesquisa é o primeiro elemento da lista cartas.
+    entrada: {
+      cartas: [4, 2, 1, -1],
+      pesquisa: 4,
+    },
+    saida: 0,
+  },
+  {
+    // a pesquisa é o último elemento da lista cartas.
+    entrada: {
+      cartas: [3, -1, -9, -127],
+      pesquisa: -127,
+    },
+    saida: 3,
+  },
+  {
+    // A lista cartas tem apenas um elemento, que é a pesquisa.
+    entrada: {
+      cartas: [6],
+      pesquisa: 6,
+    },
+    saida: 0,
+  },
+  {
+    // A lista cartas não contém a pesquisa.
+    entrada: {
+      cartas: [9, 7, 5, 2, -9],
+      pesquisa: 4,
+    },
+    saida: -1, // Como o problema não especifica essa situação, definimos um valor arbitrário
+  },
+  {
+    // A lista cartas está vazia.
+    entrada: {
+      cartas: [],
+      pesquisa: 7,
+    },
+    saida: -1,
+  },
+  {
+    // A lista cartas tem números que se repetem.
+    entrada: {
+      cartas: [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+      pesquisa: 3,
+    },
+    saida: 7,
+  },
+  {
+    // a pesquisa está em mais de uma posição na lista cartas.
+    entrada: {
+      cartas: [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+      pesquisa: 6,
+    },
+    saida: 2,
+  },
+];
 
 describe("Bateria de Testes", () => {
   for (let i = 0; i < testes.length; i++) {
@@ -6,12 +80,36 @@ describe("Bateria de Testes", () => {
 
     it(`Teste: ${i + 1}`, () => {
       const cartas = teste.entrada.cartas;
-      const numN = teste.entrada.numN;
+      const pesquisa = teste.entrada.pesquisa;
       const saida = teste.saida;
 
-      const resultado = localizarCarta(cartas, numN);
+      const resultado = localizarCarta(cartas, pesquisa);
 
       expect(resultado).toEqual(saida);
     });
   }
+
+  it("Mega teste linear", () => {
+    const cartas = Array.apply(null, Array(100000))
+      .map((_, indice) => indice)
+      .sort((a, b) => b - a);
+    const pesquisa = 2;
+    const saida = 99997;
+
+    const resultado = localizarCartaLinear(cartas, pesquisa);
+
+    expect(resultado).toEqual(saida);
+  });
+
+  it("Mega teste binário", () => {
+    const cartas = Array.apply(null, Array(100000))
+      .map((_, indice) => indice)
+      .sort((a, b) => b - a);
+    const pesquisa = 2;
+    const saida = 99997;
+
+    const resultado = localizarCarta(cartas, pesquisa);
+
+    expect(resultado).toEqual(saida);
+  });
 });
